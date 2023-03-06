45e12488e94a7e2955659ec2d08edb40c41a3dd1
Vladislav Svinarenko <vladushek12@gmail.com>
2023-03-06 19:03:10 +0300

    Change name project folder to todoproject

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -9,21 +9,24 @@ IMPORTANCE_CHOICE = [
 
 ]
 
+class Importance(models.Model):
+
+    def __str__(self):
+        pass
+
 class Task(models.Model):
     title = models.CharField(max_length=100)
     complete = models.BooleanField(default=False)
     importance = models.CharField(max_length=3, choices=IMPORTANCE_CHOICE, default='nim')
     created = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.title
-    
+
     # def get_importance_display():
     #     pass
-    
+
 # class TaskForm(ModelForm):
 #     class Meta:
 #         model = Task
 #         fields = ['title','complete','importance','created']
-
